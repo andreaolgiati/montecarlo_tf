a9b3ea5611c2fdc2053f9664c96fcd81140b45a8
Andrea Olgiati <olg@amazon.com>
2019-04-05 14:37:03 -0700

    First version

--- notebooks/inputcheck/simplemodel_infer.py
+++ notebooks/inputcheck/simplemodel_infer.py
@@ -11,29 +11,34 @@ saver = tf.train.import_meta_graph('model/model.meta')
 
 # We can now access the default graph where all our metadata has been loaded
 graph = tf.get_default_graph()
-prediction = graph.get_tensor_by_name("pred:0")
+prediction = graph.get_tensor_by_name("Y_hat:0")
 product = graph.get_tensor_by_name("product:0")
-cost = graph.get_tensor_by_name("cost:0")
+#cost = graph.get_tensor_by_name("cost:0")
 
 run_id = str(uuid.uuid4())
 
 #infer_X = np.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
 #                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-tornasole_hook = TornasoleHook("./ts_outputs/infer/",include=["^product:0$", "^X:0$", "^pred:0$"],exclude=["Y"],
+tornasole_hook = TornasoleHook("./ts_outputs/infer/",include=["^product:0$", "^X:0$", "^Y_hat:0$"],exclude=["Y"],
                     step_interval=1,
                     run_id=run_id, 
                     dry_run=False, single_file=False, 
                     local_reductions=False)
+
+batch_size = 128
 
 with tf.Session() as sess:
     # To initialize values with saved data
     saver.restore(sess, 'model/model')
     tornasole_hook.begin()
     for i in range(100):
-        infer_X = np.random.uniform(0, 10)
+        mu, sigma = 0.1*i, 2
+        infer_X = np.random.normal(mu, sigma, batch_size)
         sess_args = tornasole_hook.before_run(None)
         #print( "SA=", sess_args.feed_dict)
         results = sess.run( sess_args.fetches, feed_dict={"X:0" : infer_X } )
         tornasole_hook.after_run(None,results)
         #print( _pred, _prod )
+        if i % 10 == 0:
+            print( f'Running sample {i}')
     tornasole_hook.end(sess)
--- notebooks/inputcheck/simplemodel_train.py
+++ notebooks/inputcheck/simplemodel_train.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import uuid
@@ -8,37 +8,37 @@ from tornasole_hook import TornasoleHook
 
 run_id = str(uuid.uuid4())
 
-rng = numpy.random
+rng = np.random
 
 # Parameters
-learning_rate = 0.01
-training_epochs = 1000
-display_step = 50
+learning_rate = 0.00001
+training_epochs = 5000
+
 
 # Training Data
-train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
-n_samples = train_X.shape[0]
+#train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
+#                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
+#train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
+#                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+#n_samples = train_X.shape[0]
 
 # tf Graph Input
 X = tf.placeholder("float", name="X")
 Y = tf.placeholder("float", name="Y")
 
 # Set model weights
-W = tf.Variable(rng.randn(), name="weight")
-b = tf.Variable(rng.randn(), name="bias")
+W = tf.Variable(np.random.randn(), name="weight")
+b = tf.Variable(np.random.randn(), name="bias")
 
 # Perform the multiplication
 prod = tf.multiply(X, W, name="product")
 
 # Construct a linear model
-pred = tf.add(prod, b, name="pred")
+Y_hat = tf.add(prod, b, name="Y_hat")
 
 # Mean squared error
-cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
-cost = tf.identity(cost, name="cost")
+cost = tf.reduce_sum(tf.pow(Y_hat-Y, 2), name="cost")  #/(2*n_samples)
+#cost = tf.identity(cost, name="cost")
 
 # Gradient descent
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
@@ -47,7 +47,7 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 init = tf.global_variables_initializer()
 
 tornasole_hook = TornasoleHook("./ts_outputs/train/",include=[],exclude=[],
-                    step_interval=100,
+                    step_interval=10,
                     run_id=run_id, 
                     dry_run=False, single_file=True, 
                     local_reductions=False)
@@ -70,11 +70,22 @@ hooks=[tornasole_hook, SaveAtEnd("./model/model")]
 with tf.Session() as sess:
     sess.run(init)
 
+mu, sigma = 0, 2
+diff, mult = 1, 5
+batch_size = 32
+clip_factor = 1
+display_step = 50
 with tf.train.MonitoredTrainingSession(hooks=hooks) as sess:
     # Fit all training data
     for epoch in range(training_epochs):
-        for (x, y) in zip(train_X, train_Y):
-            sess.run(optimizer, feed_dict={X: x, Y: y})
+        #print( epoch )
+        train_X = np.random.normal(mu, sigma, batch_size)
+        
+        train_Y = train_X * mult + diff
+        #print( train_X, train_Y )
+        #train_Y = np.clip(train_Y, (diff+mu)-(sigma*mult)/clip_factor, (diff+mu)+(sigma*mult)/clip_factor)
+        #for (x, y) in zip(train_X, train_Y):
+        sess.run(optimizer, feed_dict={X: train_X, Y: train_Y})
         #Display logs per epoch step
         if (epoch+1) % display_step == 0:
             c, _W, _b = sess.run([cost, W, b], feed_dict={X: train_X, Y:train_Y})
